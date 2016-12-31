74694769ef7aad8a144525c25566ce651f34fba7
Aaron Gallagher <aaron.b.gallagher@gmail.com>
2016-12-31 13:39:46 -0600

    Refactor code

--- app/src/main/java/com/b0tchsec/makewiregreatagain/WireApp.java
+++ app/src/main/java/com/b0tchsec/makewiregreatagain/WireApp.java
@@ -27,6 +27,12 @@ import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
 
 public class WireApp implements IXposedHookLoadPackage {
 
+    static private String APP_NAME = "com.wire";
+    static private String CLASS_CALL_CTRL = "com.waz.zclient.controllers.calling.CallingController";
+    static private String METHOD_START_CALL = "startCall";
+    static private String CLASS_BASE_CONV = "com.waz.api.impl.conversation.BaseConversation";
+    static private String METHOD_KNOCK = "knock";
+
     // Replace startCall() method
     private XC_MethodReplacement startcall_hook = new XC_MethodReplacement() {
         @Override
@@ -46,17 +52,17 @@ public class WireApp implements IXposedHookLoadPackage {
     };
 
     public void handleLoadPackage(final LoadPackageParam lpparam) throws Throwable {
-        if (!lpparam.packageName.equals("com.wire"))
+        if (!lpparam.packageName.equals(APP_NAME))
             return;
 
         // Hook the method, void startCall(boolean withVideo)
-        findAndHookMethod("com.waz.zclient.controllers.calling.CallingController", lpparam.classLoader,
-                "startCall", boolean.class,
+        findAndHookMethod(CLASS_CALL_CTRL, lpparam.classLoader,
+                METHOD_START_CALL, boolean.class,
                 startcall_hook);
 
         // Hook the method, void knock()
-        findAndHookMethod("com.waz.api.impl.conversation.BaseConversation", lpparam.classLoader,
-                "knock", knock_hook);
+        findAndHookMethod(CLASS_BASE_CONV, lpparam.classLoader,
+                METHOD_KNOCK, knock_hook);
 
         XposedBridge.log("We are hooked into WireApp, time to make it great again!");
     }
